2f17041030917b2bfec1fab4e1f3d71b3a2a525a
eduardo5538 <eduardo-correa1@outlook.com.br>
2022-09-06 11:40:50 -0300

    mapa adicionado ao show inst

--- uceae-aluno/showInst.php
+++ uceae-aluno/showInst.php
@@ -99,8 +99,11 @@
    echo "<h5>" . $Res[0]['nome_escola']  . "</h5><br><br>";
    echo "<h5>" . $Res[0]['rua_escola']  . "</h5><br><br>";
    echo "<h5>" . $Res[0]['uf_escola']  . "</h5><br><br>";
+   echo "<a href='#'>" . $Res[0]['telefone_escola']  . "</a><br><br>";
 
+   
 
 ?>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3751.402513434294!2d-43.91000007038396!3d-19.90743594992827!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa69bcc8c577769%3A0xfb9fd9a312a6145d!2sAv.%207%20de%20Abril%20-%20Esplanada%2C%20Belo%20Horizonte%20-%20MG%2C%2030280-240!5e0!3m2!1ses!2sbr!4v1662474737779!5m2!1ses!2sbr" width="80%" height="750" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </body>
 </html>
