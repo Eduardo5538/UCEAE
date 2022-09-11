63c8c42fafae945ff26fc927b7c45e57c4315e07
ElectronicalVoid <davih1423@gmail.com>
2022-09-11 15:38:16 -0300

    corrigido

--- uceae-login/js/formularioAluno.js
+++ uceae-login/js/formularioAluno.js
@@ -1,7 +1,5 @@
 function passar(){
 
-
-
 const prevBtns = document.querySelectorAll(".btn-prev");
 const nextBtns = document.querySelectorAll(".btn-next");
 const progress = document.getElementById("progress");
