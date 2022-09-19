9de724f73de569870fc892b39839f41bf453701c
eduardo5538 <eduardo-correa1@outlook.com.br>
2022-09-19 10:41:03 -0300

    alterando comentarios

--- bd/bd_uceae.sql
+++ bd/bd_uceae.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 23-Ago-2022 às 06:07
+-- Tempo de geração: 30-Ago-2022 às 06:07
 -- Versão do servidor: 10.4.24-MariaDB
 -- versão do PHP: 8.1.4
 
