a85e8c7b8d6b55cb02508d9f8d0172b9d6245847
Eduardo5538 <eduardo-correa1@outlook.com.br>
2022-09-19 20:22:52 -0300

    adicionei icones as páginas

--- uceae-instituicao/cursosAdicionar.php
+++ uceae-instituicao/cursosAdicionar.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <link rel="icon" type="imagem/png" href="../img/UCEAE3.png" />
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Adicionar Cursos</title>
