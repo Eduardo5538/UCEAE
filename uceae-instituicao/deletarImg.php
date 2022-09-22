c52233e846149f902a0db47c1143d25d28909f4d
Eduardo5538 <eduardo-correa1@outlook.com.br>
2022-09-22 20:15:23 -0300

    unlink adicionado

--- uceae-instituicao/deletarImg.php
+++ uceae-instituicao/deletarImg.php
@@ -2,6 +2,15 @@
     $cod_imagem = $_GET['cod_imagem'];
     try
     {
+        $Comando = $conexao->prepare("SELECT * FROM imagens_instituicao WHERE cod_imagem = ?");
+        $Comando->bindParam(1, $cod_imagem);
+        $Comando->execute();
+        $res = $Comando->fetchAll();
+
+        $caminho = $res[0]['imagem'];
+        unlink($caminho);
+
+
         include "conection.php";
         $Comando = $conexao->prepare("DELETE FROM imagens_instituicao WHERE cod_imagem = ?");
         $Comando->bindParam(1, $cod_imagem);
