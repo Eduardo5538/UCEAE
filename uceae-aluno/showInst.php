df3c32b15fc239f0af8383f24ccb3936289a3c99
ElectronicalVoid <davih1423@gmail.com>
2022-11-07 11:25:28 -0300

    alterei dnv

--- uceae-aluno/showInst.php
+++ uceae-aluno/showInst.php
@@ -372,6 +372,31 @@
       <img src="https://assets.codepen.io/2301174/icon-karma.svg" alt="">
     </div>
   </div>
+  <br><br>
+
+  <!-- Carousel de Imagens -->
+
+  <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src=".../800x400?auto=yes&bg=777&fg=555&text=Primeiro Slide" alt="Primeiro Slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src=".../800x400?auto=yes&bg=666&fg=444&text=Segundo Slide" alt="Segundo Slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src=".../800x400?auto=yes&bg=555&fg=333&text=Terceiro Slide" alt="Terceiro Slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Anterior</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Próximo</span>
+  </a>
+</div>
 
   <!-- ---------- Comentários ---------- -->
 
