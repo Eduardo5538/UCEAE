a1c24b45162f6760a93498cdbfed29d7887e77bf
ElectronicalVoid <davih1423@gmail.com>
2022-11-07 22:03:20 -0300

    refazendo a pagina do aluno pq o edu eh bucha

--- uceae-aluno/paginaAluno.php
+++ uceae-aluno/paginaAluno.php
@@ -70,168 +70,96 @@
   </div>
   </nav>
 
-  <!-- --------------------- Área da Foto --------------------- -->
+  <!-- --------------------- Conteudo --------------------- -->
 
-  <div class="image-upload2">
-  <label for="input2" id="file">
-  <!--<form method='post' id='alter_perfil' action='alterarFoto.php' enctype="multipart/form-data">-->
-    <label for="input2">
-      <?php
-          echo "<img src='../uceae-login/".  $Res[0]['foto_aluno']  ."' id='foto_perfil'>"
-        ?>
-    </label>
-    <!--<input id="file-input2" name='imagem_perfil' type="file" / onchange='alterar_foto2()'>-->
-  <!--</form>-->
-  
-</div>
-<br><br><br>
+  <!-- Area Foto e Boas Vindas -->
 
- <!-- --------------------- Formulário do Aluno --------------------- -->
-<br><br><br><br>
-<form action="alterarAluno.php" method="post" class="form form-infoAluno" id="frm_infoAluno">
-<h1 class="titulo-forms">Informações Pessoais</h1>
-<div class="linha-forms info-pessoal">
-   <!-- Bloco de Informações -->
-  <div class="bloco-info-pessoal">
-    <div class="linha-itens">
-      <div class="bloco-itens">
-        <label for="" class="titulo-campo">Nome</label><br>
-        <input type="text" name="Nome" id="Nome" class="input-nome" placeholder='<?php echo $Res[0]['nome_aluno'] ?>'>
+  <div class="div-header">
+    <div class="linha-blocos">
+      <div class="div-foto">
+        <img src="" alt="">
       </div>
-    </div>
-    <div class="bloco-itens">
-      <label for="nasc" class="titulo-campo">Data de Nascimento</label><br>
-      <input type="date" name="nasc" id="nasc" class="input-data">
-    </div>
-    <div class="linha-itens">
-      <div class="bloco-itens">
-        <label for="def" class="titulo-campo">Possui Deficiência?</label><br>
-        <div class="linha-itens">
-          <label for="def" class="label-radio">Sim</label><input type="radio" name="def" id="Possui" class="input-def" value='possui'>
-          <label for="def" class="label-radio">Não</label><input type="radio" name="def" id="Nao Possui" class="input-def" value='não possui'>
-        </div>
-      </div>
-      <div class="bloco-itens">
-        <label for="nomeDef" class="titulo-campo">Nome da Deficiência</label><br>
-        <input type="text" name="nomeDef" id="nomeDef" class="input-sobrenome" placeholder='<?php echo $Res[0]['deficiencia'] ?>'>
-      </div>
-    </div>
-    <div class="linha-itens">
-      <div class="bloco-itens">
-        <label for="descDef" class="titulo-campo">Descrição da Deficiência</label><br>
-        <textarea name="descDef" id="descDef" class="textarea-desc" placeholder='<?php echo $Res[0]['nome_aluno'] ?>'></textarea>
+      <div class="div-titulo">
+        <h1>Boas Vindas!</h1>
+        <p>Sinta-se à vontade para editar seus dados!</p>
       </div>
     </div>
   </div>
-  <!-- Bloco de Imagem -->
-<div class="image-upload2">
-  <label for="file-input2" id="input-file">
-</div>
-</form>
-<form method='post' id='alter_fotoForm' action='alterarFoto.php' enctype="multipart/form-data" class="form-img">
-    <label for="file-input2">
-      <?php
-          echo "<img src='../uceae-login/".  $Res[0]['foto_aluno']  ."' id='foto_perfilForm'>"
-        ?>
-    </label>
-    <div id='resp2' class="btn btn-resp2"></div>
-    <input id="file-input2" name='foto_imagem' type="file" onchange='alterar_foto2()'>
-</div>
+
+  <!-- Area Formulario Alteração de Dados -->
+
+  <div class="div-form">
+    <form action="POST" name="form_alterar" id="frm_alt" class="form">
+      <div class="bloco-form div-info">
+        <div class="bloco-itens">
+          <label for="">Nome</label><br>
+          <input type="text" name="" id="">
+        </div>
+        <div class="bloco-itens">
+          <label for="">CPF</label><br>
+          <input type="text" name="" id="">
+        </div>
+        <div class="bloco-itens">
+          <label for="">Data de Nascimento</label><br>
+          <input type="date" name="" id="">
+        </div>
+        <div class="bloco-itens">
+          <label for="">Sexo</label><br>
+          <input type="radio" name="radio" id="btn_masc" value="M">
+          <label for="">Masculino</label>
+          <input type="radio" name="radio" id="btn_fem" value="F">
+          <label for="">Feminino</label>
+          <input type="radio" name="radio" id="btn_naoinforma" value="NA">
+          <label for="">Não Informado</label>
+        </div> 
+        <div class="bloco-itens">
+          <label for="">Possui Deficiência?</label><br>
+          <input type="radio" name="radio" id="btn_sim" value="S">
+          <label for="">Sim</label>
+          <input type="radio" name="radio" id="btn_nao" value="N">
+          <label for="">Não</label>
+        </div>
+        <div class="bloco-itens">
+          <label for="">Descrição da Deficiência</label><br>
+          <textarea name="" id="" cols="30" rows="10"></textarea>
+        </div>
+      </div>
+      <br><br>
+      <div class="linha-blocos">
+        <div class="bloco-form div-contato">
+          <div class="bloco-itens">
+            <label for="">Telefone</label><br>
+            <input type="tel" name="" id="">
+          </div>
+          <div class="bloco-itens">
+            <label for="">Email</label><br>
+            <input type="email" name="" id="">
+          </div>
+        </div>
+        <div class="bloco-form div-endereco">
+          <div class="bloco-itens">
+            <label for="">CEP</label><br>
+            <input type="text" name="" id="">
+          </div>
+          <div class="bloco-itens">
+            <label for="">Rua</label><br>
+            <input type="text" name="" id="">
+          </div>
+          <div class="bloco-itens">
+            <label for="">Bairro</label><br>
+            <input type="text" name="" id="">
+          </div>
+          <div class="bloco-itens">
+            <label for="">Cidade</label><br>
+            <input type="text" name="" id="">
+          </div>
+          <div class="bloco-itens">
+            <label for="">UF</label><br>
+            <input type="text" name="" id="">
+          </div>
+        </div>
+      </div>
     </form>
-<!-- Bloco mais Informações -->
-<br><br>
-<form method='post' id='maisdados' action='alterarFoto.php' enctype="multipart/form-data" class="form-img">
-<div class="linha-forms">
-  <div class="bloco-forms form-contatoAluno form">
-    <div class="div-titulo"><h1 class="titulo-forms">Mais Informações</h1></div>
-    <br>
-    <div class="bloco-itens">
-      <label for="" class="titulo-campo">Email</label><br>
-      <input type="email" name="Email" id="Email" class="input-email" placeholder='<?php echo $Res[0]['email'] ?>'>
-    </div>
-    <div class="bloco-itens">
-      <label for="" class="titulo-campo">Telefone</label><br>
-      <input type="tel" name="Telefone" id="Telefone" placeholder='<?php echo $Res[0]['telefone_aluno'] ?>' class="tel" maxlength="15">
-    </div>
-    <div class="bloco-itens">
-      <label for="" class="titulo-campo">CPF</label><br>
-      <input type="text" name="CPF" id="CPF" placeholder='<?php echo $Res[0]['CPF_aluno'] ?>' class="cpf" maxlength="14">
-    </div>
-    <div class="bloco-itens">
-      <label for="" class="titulo-campo">Senha</label><br>
-      <input type="password" name="Senha" id="Senha">
-    </div>
   </div>
-  <!-- Bloco mais Informações -->
-  <div class="bloco-forms form-enderecoAluno form">
-    <div class="div-titulo"><h1 class="titulo-forms">Endereço</h1></div>
-    <br>
-    <div class="linha-itens">
-      <div class="bloco-itens">
-        <label for="" class="titulo-campo">CEP</label><br>
-        <input type="text" name="CEP" id="CEP" class="input-cep" placeholder='<?php echo $Res[0]['cep_aluno'] ?>' maxlength="9">
-      </div>
-      <div class="bloco-itens">
-        <label for="UF" class="titulo-campo">UF</label><br>
-        <select name="UF" id="UF" class="select-uf">
-        <option value="AC" id="AC">AC</option><option value="AL" id="AL">AL</option>
-                  <option value="AP" id="AP">AP</option><option value="AM" id="AM">AM</option>
-                  <option value="BA" id="BA">BA</option><option value="CE" id="CE">CE</option>
-                  <option value="DF" id="DF">DF</option><option value="ES" id="ES">ES</option>
-                  <option value="GO" id="GO">GO</option><option value="MA" id="MA">MA</option>
-                  <option value="MT" id="MT">MT</option><option value="MS" id="MS">MS</option>
-                  <option value="MG" id="MG">MG</option><option value="PA" id="PA">PA</option>
-                  <option value="PB" id="PB">PB</option><option value="PR" id="PR">PR</option>
-                  <option value="PE" id="PE">PE</option><option value="PI" id="PI">PI</option>
-                  <option value="RJ" id="RJ">RJ</option><option value="RN" id="RN">RN</option>
-                  <option value="RS" id="RS">RS</option><option value="RO" id="RO">RO</option>
-                  <option value="RR" id="RR">RR</option><option value="SC" id="SC">SC</option>
-                  <option value="SP" id="SP">SP</option><option value="SE" id="SE">SE</option>
-                  <option value="TO" id="TO">TO</option>
-        </select>
-      </div>
-    </div>  
-    <div class="bloco-itens">
-      <label for="" class="titulo-campo" >Rua</label><br>
-      <input type="text" name="Rua" id="Rua" placeholder='<?php echo $Res[0]['rua_aluno'] ?>'>
-    </div>
-    <div class="bloco-itens">
-      <label for="" class="titulo-campo" >Bairro</label><br>
-      <input type="text" name="Bairro" id="Bairro" placeholder='<?php echo $Res[0]['bairro_aluno'] ?>'>
-    </div>
-    <div class="bloco-itens">
-      <label for="" class="titulo-campo">Cidade</label><br>
-      <input type="text" name="Cidade" id="Cidade" placeholder='<?php echo $Res[0]['cidade_aluno'] ?>'>
-    </div>
-    <br>
-    </div>
-    <br>
-    </div>
-    <div class="button-area"><input type="button" value="Confirmar Alterações" class="button-alterar" id="btn_alterar" onclick='alterData()'></div>
-    </form>
-
-<br><br>
- <!-- --------------------- Modal --------------------- -->
-
-  <div class="modal fade" id="ModalSair" tabindex="-1" aria-labelledby="ModalLabelSair" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content">
-        <div class="modal-header text-center">
-          <h5 class="modal-title entrar-titulo w-100" id="ModalLabelSair"><?php echo $Res[0]['nome_aluno']; ?></h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body" style="margin: 0 25% 0 25%;">
-           <?php echo "<img src='../uceae-login/".  $Res[0]['foto_aluno']  ."' id='ft_perfil_modal'>"?><br><br>
-           <h3><?php echo $Res[0]['nome_aluno']; ?></h3><br>
-           <label><?php echo $Res[0]['email']; ?></label>
-        </div>
-              <div class="modal-footer justify-content-center">
-              <a href='../uceae-login/unlogin.php' class='btn btn-danger' tabindex='-1' role='button' aria-disabled='true'>Sair</a>
-              <a href='../uceae-instituicao/paginaInst.php' class='btn btn-warning' tabindex='-1' role='button' style='margin-right: 10px;' aria-disabled='true'>Sua página!</a>
-              </div>
-      </div>
-    </div>
-  </div>
-
 </body>
 </html>
